772b7ececa130106c9c6ac66c81ec2a17137172a
Woodruri <woodruff.l.riley@gmail.com>
2023-02-28 09:52:55 -0800

    Added a bit

--- snake.h
+++ snake.h
@@ -1,11 +1,23 @@
 //Made by Riley
-
 #pragma once
+
+#include "stdio.h"
+#include "conio.h"
+#include "stdlib.h"
+#include "time.h"
+#include "ctime.h"
+#include "windows.h"
+#include "process.h"
 
 #define WIDTH 40
 #define HEIGHT 40
-struct sGame {
 
+struct vec {
+    int x;
+    int y;
+    int direction;
+};
+struct snake {
     int len;
+};
 
-} sGame;
